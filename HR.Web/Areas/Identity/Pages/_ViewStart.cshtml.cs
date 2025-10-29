83c248d63a61a5ffbc415b213cc9a71e38de7987
chathnindu-ehealthcare <chathnindu@ehealthcare.lk>
2025-10-29 14:15:17 +0530

    chore: Added new files and info

---
+++ HR.Web/Areas/Identity/Pages/_ViewStart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HR.Web.Areas.Identity.Pages.Account
+{
+    public class _ViewStartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
